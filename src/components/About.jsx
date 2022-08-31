a0d827eb55adce8ff07323768477f0c05880198e
Shivu-a <sebaingame@gmail.com>
2022-08-31 11:13:55 -0300

    added skills

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ export const About = ({ language }) => {
   const { title, description } = language;
 
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center gap-4 p-4 lg:mx-[10vw]">
+    <div className="w-full h-full flex flex-col justify-center items-center gap-4 p-4 lg:ml-[5vw]">
       <h2 className="text-[#496F69] font-extrabold text-4xl">{title}</h2>
       <p>{description}</p>
     </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-export const Skills = () => {
-  return <div>Skills</div>;
+export const Skills = ({ language }) => {
+  const { title, description, softSkillsDescription, softSkills } = language;
+
+  return (
+    <div className="w-full h-full flex flex-col lg:ml-[5vw] justify-center items-center">
+      <h1 className="text-[#496F69] font-extrabold text-4xl">{title}</h1>
+      <p>{description}</p>
+    </div>
+  );
 };
