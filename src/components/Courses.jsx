94d02526088cb5a81263b74c778194efd047d49a
Shivu-a <sebaingame@gmail.com>
2022-08-31 19:43:15 -0300

    updated language.json

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-export const Courses = () => {
-  return <div>Courses</div>;
+export const Courses = ({ language }) => {
+  const { title, description, coursesArray } = language;
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </div>
+  );
 };
