0d4f02a331c9edf522bf36bbd405fea6da4fd121
Shivu-a <sebaingame@gmail.com>
2022-08-31 19:37:55 -0300

    updated language.json

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,18 +7,23 @@ export const Skills = ({ language }) => {
   return (
     <div className="w-full h-full flex flex-col pb-[10vh] lg:pb-0 lg:ml-[5vw] pt-4 items-center overflow-auto">
       <h1 className="text-[#496F69] font-extrabold text-4xl">{title}</h1>
-      <p className="p-4">{description}</p>
-      <div className="flex flex-wrap justify-center items-baseline p-4 lg:gap-4">
-        {skillsArray.map((skill) => (
-          <div
-            key={skill.title}
-            className="flex flex-col items-center w-1/2 lg:w-1/5 mt-4 lg:mt-0 hover:text-[#496F69] transition-all"
-          >
-            <img className="w-full" src={skill.imgLink} alt={skill.title} />
-            <span className="text-lg mt-2 font-semibold">{skill.title}</span>
-          </div>
-        ))}
+
+      <div className="flex flex-col h-[40vh] lg:flex-row justify-center items-center">
+        <p className="p-4">{description}</p>
+
+        <div className="flex flex-wrap justify-center items-baseline h-full p-4 lg:p-0 lg:gap-4">
+          {skillsArray.map((skill) => (
+            <div
+              key={skill.title}
+              className="flex flex-col items-center w-1/2 lg:w-[9vw] mt-4 lg:mt-0 hover:text-[#496F69] transition-all"
+            >
+              <img className="w-full" src={skill.imgLink} alt={skill.title} />
+              <span className="text-lg mt-2 font-semibold">{skill.title}</span>
+            </div>
+          ))}
+        </div>
       </div>
+
       <div className="flex flex-col items-center">
         <p className="p-4">{softSkillsDescription}</p>
         <div className="flex justify-center p-4 flex-wrap lg:w-1/2 gap-4">
