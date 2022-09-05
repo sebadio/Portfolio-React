32eb05ac64c410250cd66f11a1e0eb21e1b5cc1d
Shivu-a <sebaingame@gmail.com>
2022-09-05 17:15:56 -0300

    Fixed InteractiveDiv.jsx width in large mode

--- src/components/InteractiveDiv.jsx
+++ src/components/InteractiveDiv.jsx
@@ -4,8 +4,8 @@ export const InteractiveDiv = ({ image, title, handleShowModal, type }) => {
   return (
     <div
       onClick={handleShowModal}
-      className={`w-full lg:${
-        type === "projects" ? "w-1/3" : "w-1/4"
+      className={`w-full ${
+        type === "projects" ? "lg:w-1/3" : "lg:w-1/4"
       } relative overflow-hidden border-[#B2A7B8] border-[1px] hover:border-[#496f6960] shadow-md shadow-black transition-all
             after:flex after:justify-center after:items-center after:content-[attr(after)] after:bottom-0 left-0 after:w-full after:h-1/3 after:absolute after:text-white after:font-bold after:text-center after:text-lg lg:after:text-2xl ${
               !window.matchMedia("(any-hover: none)").matches &&
