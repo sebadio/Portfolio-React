f5a78053f557166dcea9a3bdece6810818af2297
Shivu-a <sebaingame@gmail.com>
2022-10-01 23:29:58 -0300

    updated docs

---
+++ docs/assets/animation.565a44f8.js
@@ -0,0 +1,1 @@
+const a={initial:{opacity:0},animate:{opacity:1},exit:{opacity:0}};export{a};
---
+++ docs/assets/getOffset.54eb2b20.js
@@ -0,0 +1,1 @@
+const o=t=>{const e=t.target.offsetTop;return{left:t.target.offsetLeft,top:e}};export{o as g};
